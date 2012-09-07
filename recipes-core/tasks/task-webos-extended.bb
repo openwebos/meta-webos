openwebos/meta-webos

commit 241dce90ab79a2a6df9ec9a5b62efbaa2b37517e

removed tabs from bbfiles

recipes-core/tasks/task-webos-extended.bb
@@ -17,23 +17,24 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 ALLOW_EMPTY = "1"
 
 RDEPENDS_${PN} = " \
-	nyx-lib \
-	cjson \
-	pmloglib \
-	luna-service2 \
-	powerd \
-	sleepd \
-	storaged \
-        luna-init \
-        jemalloc \
-        librdx-stub \
-        qt4-palm \
-        pmcertificatemgr \
-        librolegen \
-        pmstatemachineengine \
-        libpalmsocket \
-        libsandbox \
-        glibcurl \
+    nyx-lib \
+    cjson \
+    pmloglib \
+    luna-service2 \
+    powerd \
+
+    sleepd \
+    storaged \
+    luna-init \
+    jemalloc \
+    librdx-stub \
+    qt4-palm \
+    pmcertificatemgr \
+    librolegen \
+    pmstatemachineengine \
+    libpalmsocket \
+    libsandbox \
+    glibcurl \
 "
 #        luna-prefs ( broken )
 #        luna-universalsearchmgr ( broken )

recipes-upstreamable/cmake/cmake_2.8.7.bb
@@ -20,11 +20,11 @@ python () {
     docdir=d.getVar("docdir", True)
 
     if not docdir.startswith(prefix):
-	raise bb.build.FuncFailed('docdir must contain prefix as its prefix')
+    raise bb.build.FuncFailed('docdir must contain prefix as its prefix')
 
     docdir_stripped = docdir[len(prefix):]
     if len(docdir_stripped) > 0 and docdir_stripped[0] == '/':
-	docdir_stripped = docdir_stripped[1:]
+    docdir_stripped = docdir_stripped[1:]
 
     d.setVar("docdir_stripped", docdir_stripped)
 }

recipes-core/tasks/task-bsp-webos.bb
@@ -17,10 +17,10 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 ALLOW_EMPTY = "1"
 
 RDEPENDS_${PN} = " \
-	base-files \
-	initscripts \
-	udev  \
-	update-rc.d  \
-	upstart-initscripts \
-        netbase \
+    base-files \
+    initscripts \
+    udev  \
+    update-rc.d  \
+    upstart-initscripts \
+    netbase \
 "

recipes-core/tasks/task-webos-core.bb
@@ -26,7 +26,7 @@ RDEPENDS_${PN} = " \
      bzip2 \
      c-ares \
      db \
-     e2fsprogs \	  		
+     e2fsprogs \
      freetype \
      giflib \
      glib-2.0 \

recipes-extended/psmisc/psmisc_22.19.bbappend
@@ -8,7 +8,7 @@ PACKAGES = "fuser killall pstree \
 "
 
 do_configure_prepend() {
-	sed -i '/^SUBDIRS/s/=.*$/= src/' Makefile.am
-	sed -i '/^oldfuser_/s/^/#/' src/Makefile.am
-	sed -i '/^bin_PROGRAMS/s/ oldfuser//' src/Makefile.am
+        sed -i '/^SUBDIRS/s/=.*$/= src/' Makefile.am
+        sed -i '/^oldfuser_/s/^/#/' src/Makefile.am
+        sed -i '/^bin_PROGRAMS/s/ oldfuser//' src/Makefile.am
 }

recipes-support/e2fsprogs/e2fsprogs_1.42.1.bbappend
@@ -5,7 +5,7 @@ FILESEXTRAPATHS_prepend := "${THISDIR}"
 SRC_URI += "file://e2fsck.conf"
 
 do_install_append() {
-	install -d ${D}${sysconfdir}
-	install -m 0644 ${WORKDIR}/e2fsck.conf ${D}/${sysconfdir}/e2fsck.conf
+        install -d ${D}${sysconfdir}
+        install -m 0644 ${WORKDIR}/e2fsck.conf ${D}/${sysconfdir}/e2fsck.conf
 }
 

recipes-support/libgcrypt/libgcrypt_1.5.0.bbappend
@@ -5,10 +5,10 @@ PR = "webos1"
 EXTRA_OECONF =+ " --disable-static"
 
 do_configure_prepend() {
-	sed -i '/^m4datadir/s,$,/${PN},' src/Makefile.am
+        sed -i '/^m4datadir/s,$,/${PN},' src/Makefile.am
 }
 
 do_configure_append() {
-	sed -i '/^SUBDIRS/s/ doc tests//' Makefile
+        sed -i '/^SUBDIRS/s/ doc tests//' Makefile
 }
 

recipes-upstreamable/c-ares/c-ares_1.7.4.bb
@@ -17,8 +17,8 @@ PR = "r1"
 EXTRA_OECONF = "--enable-shared"
 
 do_install_append() {
-	install -d ${D}/${includedir}/ares
-	install -m 0644 ares*.h ${D}/${includedir}/ares/
+    install -d ${D}/${includedir}/ares
+    install -m 0644 ares*.h ${D}/${includedir}/ares/
 }
 
 do_configure_append() {

commit b0f14e1484a1ca0d73181b0dea08d0eb69ca8e92

Fix whitespace errors introduced in commit 241dce90ab79a2a6df9e
Open-webOS-DCO-1.0-Signed-off-by: Kimmo Leppala <kimmo.leppala@palm.com>

Change-Id: Ia285ec6037355155af0814c1cfcd1fb71678325d

recipes-core/tasks/task-webos-extended.bb
@@ -22,7 +22,6 @@ RDEPENDS_${PN} = " \
     pmloglib \
     luna-service2 \
     powerd \
-
     sleepd \
     storaged \
     luna-init \

recipes-upstreamable/cmake/cmake_2.8.7.bb
@@ -20,11 +20,11 @@ python () {
     docdir=d.getVar("docdir", True)
 
     if not docdir.startswith(prefix):
-    raise bb.build.FuncFailed('docdir must contain prefix as its prefix')
+        raise bb.build.FuncFailed('docdir must contain prefix as its prefix')
 
     docdir_stripped = docdir[len(prefix):]
     if len(docdir_stripped) > 0 and docdir_stripped[0] == '/':
-    docdir_stripped = docdir_stripped[1:]
+        docdir_stripped = docdir_stripped[1:]
 
     d.setVar("docdir_stripped", docdir_stripped)
 }
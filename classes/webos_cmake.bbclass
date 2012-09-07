openwebos/meta-webos

commit 241dce90ab79a2a6df9ec9a5b62efbaa2b37517e

removed tabs from bbfiles

recipes-core/tasks/task-webos-extended.bb
@@ -17,23 +17,24 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 ALLOW_EMPTY = "1"
 
 RDEPENDS_${PN} = " \
-	nyx-lib \
-	cjson \
-	pmloglib \
-	luna-service2 \
-	powerd \
-	sleepd \
-	storaged \
-        luna-init \
-        jemalloc \
-        librdx-stub \
-        qt4-palm \
-        pmcertificatemgr \
-        librolegen \
-        pmstatemachineengine \
-        libpalmsocket \
-        libsandbox \
-        glibcurl \
+    nyx-lib \
+    cjson \
+    pmloglib \
+    luna-service2 \
+    powerd \
+
+    sleepd \
+    storaged \
+    luna-init \
+    jemalloc \
+    librdx-stub \
+    qt4-palm \
+    pmcertificatemgr \
+    librolegen \
+    pmstatemachineengine \
+    libpalmsocket \
+    libsandbox \
+    glibcurl \
 "
 #        luna-prefs ( broken )
 #        luna-universalsearchmgr ( broken )

recipes-upstreamable/cmake/cmake.inc
@@ -19,5 +19,5 @@ SRC_URI = "http://www.cmake.org/files/v${CMAKE_MAJOR_VERSION}/cmake-${PV}.tar.gz
 inherit autotools
 
 do_configure () {
-	./configure --prefix=${prefix}
+    ./configure --prefix=${prefix}
 }

recipes-upstreamable/cmake/cmake_2.8.7.bb
@@ -20,11 +20,11 @@ python () {
     docdir=d.getVar("docdir", True)
 
     if not docdir.startswith(prefix):
-	raise bb.build.FuncFailed('docdir must contain prefix as its prefix')
+    raise bb.build.FuncFailed('docdir must contain prefix as its prefix')
 
     docdir_stripped = docdir[len(prefix):]
     if len(docdir_stripped) > 0 and docdir_stripped[0] == '/':
-	docdir_stripped = docdir_stripped[1:]
+    docdir_stripped = docdir_stripped[1:]
 
     d.setVar("docdir_stripped", docdir_stripped)
 }

recipes-webos/cmake-modules-webos/cmake-modules-webos.bb
@@ -34,7 +34,7 @@ SRC_URI = "${OPENWEBOS_GIT_REPO}/cmake-modules-webos;tag=${WEBOS_GIT_TAG};protoc
 S = "${WORKDIR}/git"
 
 do_compile() {
-    	:
+     :
 }
 
 #do_install() {
@@ -44,7 +44,7 @@ do_compile() {
 #}
 
 do_clean_append() {
-	buildpath = bb.data.getVar('OECMAKE_BUILDPATH', d, 1)
+        buildpath = bb.data.getVar('OECMAKE_BUILDPATH', d, 1)
         if buildpath and os.path.exists(buildpath):
             bb.note('removing ' + buildpath)
             os.system('rm -rf ' + buildpath)

recipes-core/tasks/task-bsp-webos.bb
@@ -17,10 +17,10 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 ALLOW_EMPTY = "1"
 
 RDEPENDS_${PN} = " \
-	base-files \
-	initscripts \
-	udev  \
-	update-rc.d  \
-	upstart-initscripts \
-        netbase \
+    base-files \
+    initscripts \
+    udev  \
+    update-rc.d  \
+    upstart-initscripts \
+    netbase \
 "

recipes-core/tasks/task-webos-core.bb
@@ -26,7 +26,7 @@ RDEPENDS_${PN} = " \
      bzip2 \
      c-ares \
      db \
-     e2fsprogs \	  		
+     e2fsprogs \
      freetype \
      giflib \
      glib-2.0 \

recipes-extended/psmisc/psmisc_22.19.bbappend
@@ -8,7 +8,7 @@ PACKAGES = "fuser killall pstree \
 "
 
 do_configure_prepend() {
-	sed -i '/^SUBDIRS/s/=.*$/= src/' Makefile.am
-	sed -i '/^oldfuser_/s/^/#/' src/Makefile.am
-	sed -i '/^bin_PROGRAMS/s/ oldfuser//' src/Makefile.am
+        sed -i '/^SUBDIRS/s/=.*$/= src/' Makefile.am
+        sed -i '/^oldfuser_/s/^/#/' src/Makefile.am
+        sed -i '/^bin_PROGRAMS/s/ oldfuser//' src/Makefile.am
 }

recipes-support/e2fsprogs/e2fsprogs_1.42.1.bbappend
@@ -5,7 +5,7 @@ FILESEXTRAPATHS_prepend := "${THISDIR}"
 SRC_URI += "file://e2fsck.conf"
 
 do_install_append() {
-	install -d ${D}${sysconfdir}
-	install -m 0644 ${WORKDIR}/e2fsck.conf ${D}/${sysconfdir}/e2fsck.conf
+        install -d ${D}${sysconfdir}
+        install -m 0644 ${WORKDIR}/e2fsck.conf ${D}/${sysconfdir}/e2fsck.conf
 }
 

commit d99987277d6460a6f31feea9ec025bfffdefe111

Fixing build problems introduced by commit 241dce90ab79a2a6df9ec9a5b62efbaa2b37517e.
Signed-off-by: Iyad Qumei <iyadkq@gmail.com>

classes/webos_cmake.bbclass
@@ -37,17 +37,15 @@ EXTRA_OECMAKE += "${@ '-DWEBOS_TARGET_MACHINE_IMPL:STRING=${WEBOS_TARGET_MACHINE
 EXTRA_OEMAKE += "-C ${OECMAKE_BUILDPATH}"
 
 do_configure() {
-	cmake_do_configure
+        cmake_do_configure
 }
 
 do_compile() {
-    	#oe_runmake all
         cmake_do_compile
 }
 
 do_install() {
-	# oe_runmake DESTDIR="${D}" install
-	cmake_do_install
+        cmake_do_install
 }
 
 # Stage headers and libraries that are installed by CMakeLists.txt with the 
@@ -59,12 +57,12 @@ do_install() {
 #}
 
 do_generate_toolchain_file() {
- 	cmake_do_generate_toolchain_file
+        cmake_do_generate_toolchain_file
 }
 
 do_clean_append() {
-	buildpath = bb.data.getVar('OECMAKE_BUILDPATH', d, 1)
-	if buildpath and os.path.exists(buildpath):
-		bb.note('removing ' + buildpath)
-		os.system('rm -rf ' + buildpath)
+        buildpath = bb.data.getVar('OECMAKE_BUILDPATH', d, 1)
+        if buildpath and os.path.exists(buildpath):
+            bb.note('removing ' + buildpath)
+            os.system('rm -rf ' + buildpath)
 }

classes/webos_enhanced_submissions.bbclass
@@ -7,31 +7,31 @@
 # PV is the first underscore-separated field in PREFERRED_VERSION_<packagename>,
 # i.e., it includes the submission.
 def webos_enhsub_get_pv(pn, d):
-	import bb
+        import bb
 
-	preferred_v = bb.data.getVar('PREFERRED_VERSION_' + pn, d, 1) or '0'
-	return preferred_v.split('_')[0]
+        preferred_v = bb.data.getVar('PREFERRED_VERSION_' + pn, d, 1) or '0'
+        return preferred_v.split('_')[0]
 
 # The component version is PREFERRED_VERSION_<packagename> with the last hyphen-
 # separated field removed; i.e., it does not include the submission.
 def webos_enhsub_get_component_version(pn, d):
-	import bb
+        import bb
 
-	preferred_v = bb.data.getVar('PREFERRED_VERSION_' + pn, d, 1) or '0'
-	split_preferred_v = preferred_v.split('-')
-	if len(split_preferred_v) == 1:
-		# If there's no submission, then the component version can't
-		# contain a hyphen
-		return preferred_v.split('_')[0]
-	return "-".join(split_preferred_v[:-1])
+        preferred_v = bb.data.getVar('PREFERRED_VERSION_' + pn, d, 1) or '0'
+        split_preferred_v = preferred_v.split('-')
+        if len(split_preferred_v) == 1:
+                # If there's no submission, then the component version can't
+                # contain a hyphen
+                return preferred_v.split('_')[0]
+        return "-".join(split_preferred_v[:-1])
 
 # The submission is the first underscore-separated field in an enhanced
 # submission value.
 def webos_enhsub_get_submission(pn, d):
-	import bb
+        import bb
 
-	enhanced_submission = bb.data.getVar('SUBMISSION_' + pn, d, 1) or '0'
-	return enhanced_submission.split('_')[0]
+        enhanced_submission = bb.data.getVar('SUBMISSION_' + pn, d, 1) or '0'
+        return enhanced_submission.split('_')[0]
 
 
 PV = "${@webos_enhsub_get_pv('${PN}', d)}"

recipes-core/tasks/task-webos-extended.bb
@@ -22,7 +22,6 @@ RDEPENDS_${PN} = " \
     pmloglib \
     luna-service2 \
     powerd \
-
     sleepd \
     storaged \
     luna-init \

recipes-upstreamable/cmake/cmake.inc
@@ -19,5 +19,5 @@ SRC_URI = "http://www.cmake.org/files/v${CMAKE_MAJOR_VERSION}/cmake-${PV}.tar.gz
 inherit autotools
 
 do_configure () {
-    ./configure --prefix=${prefix}
+	./configure --prefix=${prefix}
 }

recipes-upstreamable/cmake/cmake_2.8.7.bb
@@ -20,11 +20,11 @@ python () {
     docdir=d.getVar("docdir", True)
 
     if not docdir.startswith(prefix):
-    raise bb.build.FuncFailed('docdir must contain prefix as its prefix')
+	raise bb.build.FuncFailed('docdir must contain prefix as its prefix')
 
     docdir_stripped = docdir[len(prefix):]
     if len(docdir_stripped) > 0 and docdir_stripped[0] == '/':
-    docdir_stripped = docdir_stripped[1:]
+	docdir_stripped = docdir_stripped[1:]
 
     d.setVar("docdir_stripped", docdir_stripped)
 }

recipes-webos/cmake-modules-webos/cmake-modules-webos.bb
@@ -37,12 +37,6 @@ do_compile() {
      :
 }
 
-#do_install() {
-#	# Do not specify DESTDIR here. The install file uses absolute paths
-#	# to find the cmake installation in use.
-#	oe_runmake install
-#}
-
 do_clean_append() {
         buildpath = bb.data.getVar('OECMAKE_BUILDPATH', d, 1)
         if buildpath and os.path.exists(buildpath):

recipes-webos/qt4/qt4-palm.inc
@@ -22,7 +22,7 @@ def qt4_machine_config_arch_lite(bb, d):
         if this_machine == "qemux86":
             return "-embedded x86"
         elif this_machine == "qemuarm":
-	    return "-arch arm -embedded"
+            return "-arch arm -embedded"
         else:
             return "-arch arm -qpa"
     else:
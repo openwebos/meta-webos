openwebos/meta-webos

commit 70b787bcb78db34c6a7d05b19786cb2e48bbece2

task-webos-extended: Stop using RDEPENDS as a pseudo-BOM
Use it as OE intends it to be used: as a list of top-level packages;
i.e., don't include any that are a dependency of another. A side effect
of this change is that task-webos-core.bb is no longer needed, since
its sole purpose is to list the packages from openembedded-core and
meta-oe that are used by the Palm-developed components in
task-webos-extended.bb .

Open-webOS-DCO-1.0-Signed-off-by: Herb Kuta <herb.kuta@palm.com>
Change-Id: I99fd599abde0b900f89a0d98e704d17156c7b763

recipes-core/tasks/task-webos-extended.bb
@@ -4,7 +4,7 @@ DESCRIPTION = "meta-webos components used in Open webOS"
 LICENSE = "Apache-2.0"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
 
-PR = "r3"
+PR = "r4"
 
 PACKAGES = "\
     ${PN} \
@@ -17,72 +17,43 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 ALLOW_EMPTY = "1"
 
 RDEPENDS_${PN} = " \
-    adapterbase \
-    browser-adapter \
-    browserserver \
     activitymanager \
     app-services \
-    cjson \
+    browser-adapter \
     com.palm.app.browser \
     configurator \
     core-apps \
     cpushareholder-stub \
-    db8 \
     enyo-1.0 \
     fbprogress \
     filecache \
     foundation-frameworks \
-    glibcurl \
-    jemalloc \
-    libpalmsocket \
-    libpbnjson \
     librdx-stub \
-    librolegen \
-    libsandbox \
     loadable-frameworks \
     luna-applauncher \
     luna-init \
-    luna-prefs \
-    luna-service2 \
     luna-sysmgr \
-    luna-sysmgr-ipc \
-    luna-sysmgr-ipc-messages \
     luna-sysservice \
     luna-systemui \
     luna-universalsearchmgr \
-    luna-webkit-api \
-    mojoloader \
     mojolocation-stub \
-    mojomail-common \
     mojomail-imap \
     mojomail-pop \
     mojomail-smtp \
-    mojoservicelauncher \
     mojoservice-frameworks \
-    nodejs \
-    nodejs-module-webos-dynaload \
-    nodejs-module-webos-pmlog \
-    nodejs-module-webos-sysbus \
-    npapi-headers \
-    nyx-lib \
-    nyx-modules \
-    pmcertificatemgr \
+    mojoservicelauncher \
     pmklogd \
-    pmlogconf \
-    pmlogdaemon \
-    pmloglib \
     pmlogctl \
+    pmlogdaemon \
     pmnetconfigmanager-stub \
-    pmstatemachineengine \
-    powerd \
     rdx-utils-stub \
-    qt4-webos \
     sleepd \
     storaged \
     underscore \
-    webkit-supplemental \
-    webkit-webos \
     webos-initscripts \
     webos-shutdownscripts \
 "
-#        serviceinstaller (todo)
+
+# Unused components:
+# glibcurl
+# pmcertificatemgr

classes/webos-image.bbclass
@@ -25,7 +25,6 @@ LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/Apache-2.0;md5=89aea4e17d99a7ca
 #
 
 PACKAGE_GROUP_webos-boot = "task-webos-boot"
-PACKAGE_GROUP_webos-core = "task-webos-core"
 PACKAGE_GROUP_webos-extended = "task-webos-extended"
 PACKAGE_GROUP_webos-internal = "task-webos-internal"
 PACKAGE_GROUP_webos-test = "task-webos-test"
@@ -41,7 +40,6 @@ PACKAGE_GROUP_package-management = "${ROOTFS_PKGMANAGE}"
 
 WEBOS_IMAGE_BASE_INSTALL = '\
     task-webos-boot \
-    task-webos-core \
     task-webos-extended \
     \
     ${WEBOS_IMAGE_EXTRA_INSTALL} \

recipes-core/tasks/task-webos-core.bb
@@ -1,71 +1,0 @@
-# (c) Copyright 2012  Hewlett-Packard Development Company, L.P. 
-
-DESCRIPTION = "openembedded-core and meta-oe components used in Open webOS"
-LICENSE = "Apache-2.0"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
-
-PR = "r4"
-
-PACKAGES = "\
-    ${PN} \
-    ${PN}-dbg \
-    ${PN}-dev \
-    "
-
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-
-ALLOW_EMPTY = "1"
-
-RDEPENDS_${PN} = " \
-     base-files \
-     base-passwd \  
-     bash \
-     binutils \
-     boost \
-     busybox \
-     bzip2 \
-     c-ares \
-     curl \
-     db \
-     dhcp-client \
-     dropbear \
-     e2fsprogs \
-     freetype \
-     giflib \
-     glib-2.0 \
-     glibmm \
-     gzip \
-     hunspell \
-     icu \
-     iproute2 \
-     jpeg \
-     libgcrypt \
-     libgpg-error \
-     libpng \
-     libtinyxml \
-     libtool \
-     libxml2 \
-     libxslt \
-     makedevs \
-     module-init-tools \
-     modutils-initscripts \
-     ncurses \
-     netbase \
-     openssl \
-     opkg \
-     orc \
-     parted \
-     procps \
-     psmisc \
-     ptmalloc3 \
-     readline \
-     sqlite3 \
-     sysvinit-pidof \
-     tzdata \
-     update-modules \
-     update-rc.d \
-     upstart-sysvcompat \
-     uriparser \
-     yajl \
-     zlib \
-"

commit 8349032857f0022d88dca7684dbd92a07d6d502b

task-webos-extended: Restore packages dropped from webos-image
Commit 70b787bcb78db34c6a7d05b19786cb2e48bbece2 caused a number of
packages to no longer be included in webos-image because they are
missing from the RDEPENDS lists of the components that expect them to
be present at runtime. Until they can be added there, place them
explicitly in the RDEPENDS_${PN} list of task-webos-extended.

Open-webOS-DCO-1.0-Signed-off-by: Herb Kuta <herb.kuta@palm.com>
Change-Id: I5ae5785f6b853e1eff35dc7f72ae5877ab563184

recipes-core/tasks/task-webos-extended.bb
@@ -4,13 +4,13 @@ DESCRIPTION = "meta-webos components used in Open webOS"
 LICENSE = "Apache-2.0"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
 
-PR = "r4"
+PR = "r5"
 
 PACKAGES = "\
     ${PN} \
     ${PN}-dbg \
     ${PN}-dev \
-    "
+"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
@@ -23,13 +23,9 @@ RDEPENDS_${PN} = " \
     com.palm.app.browser \
     configurator \
     core-apps \
-    cpushareholder-stub \
     enyo-1.0 \
-    fbprogress \
     filecache \
-    foundation-frameworks \
     librdx-stub \
-    loadable-frameworks \
     luna-applauncher \
     luna-init \
     luna-sysmgr \
@@ -40,20 +36,67 @@ RDEPENDS_${PN} = " \
     mojomail-imap \
     mojomail-pop \
     mojomail-smtp \
-    mojoservice-frameworks \
     mojoservicelauncher \
     pmklogd \
     pmlogctl \
     pmlogdaemon \
     pmnetconfigmanager-stub \
-    rdx-utils-stub \
     sleepd \
     storaged \
-    underscore \
     webos-initscripts \
     webos-shutdownscripts \
+    ${WEBOS_MISSING_FROM_RDEPENDS} \
+    ${WEBOS_FOSS_MISSING_FROM_RDEPENDS} \
 "
 
-# Unused components:
-# glibcurl
-# pmcertificatemgr
+# XXX These non-top-level components must be explicitly added because they are
+# missing from the RDEPENDS lists of the components that expect them to be
+# present at runtime.
+WEBOS_MISSING_FROM_RDEPENDS = " \
+    browserserver \
+    cpushareholder-stub \
+    fbprogress \
+    foundation-frameworks \
+    loadable-frameworks \
+    mojoservice-frameworks \
+    nodejs \
+    rdx-utils-stub \
+    underscore \
+    webkit-supplemental \
+    webkit-webos \
+"
+
+# XXX These FOSS components must be explicitly added because they are missing
+# from the RDEPENDS lists of the components that expect them to be present at
+# runtime (or perhaps some are in fact top-level components and some others
+# aren't actually needed).
+WEBOS_FOSS_MISSING_FROM_RDEPENDS = " \
+    bash \
+    binutils \
+    bzip2 \
+    curl \
+    dhcp-client \
+    dropbear \
+    e2fsprogs \
+    gzip \
+    hunspell \
+    icu \
+    iproute2 \
+    makedevs \
+    ncurses \
+    openssl \
+    opkg \
+    parted \
+    procps \
+    psmisc \
+    sqlite3 \
+    sysvinit-pidof \
+    tzdata \
+"
+
+# Unused meta-webos components:
+# - glibcurl
+# - libtinyxml
+
+# Unused meta-oe components:
+# - orc